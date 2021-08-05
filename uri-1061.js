1265f73072617f3282ae439491362264a05c5636
unknown <josevictormobile@gmail.com>
2021-08-05 00:24:33 -0300

    Exercício URI 1061 - Finalizado e Aceito

--- uri-1061.js
+++ uri-1061.js
@@ -1,24 +1,71 @@
-var diaInicio = "Dia 5";
+//ENTRADAS
 
-var horaInicio = "08 : 12 : 23";
+// Input Sample	
+// Dia 5
+// 08 : 12 : 23
+// Dia 9
+// 06 : 13 : 23
 
-var diaFinal = "Dia 9";
+// Output Sample
+// 3 dia(s)
+// 22 hora(s)
+// 1 minuto(s)
+// 0 segundo(s)
 
-var horaFinal = "06 : 13 : 23";
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 
+var diaInicio = lines.shift();
+var horaInicio = lines.shift();
+var diaFinal = lines.shift();
+var horaFinal = lines.shift();
+
+//CAPTURA DOS DADOS QUE QUANTIFICAM TEMPO NAS ENTRADAS DE INICIO
 var hora = horaInicio[0]+horaInicio[1];
 var minuto = horaInicio[5]+horaInicio[6];
 var segundo = horaInicio[10]+horaInicio[11];
 
 var dia = diaInicio[4]+diaInicio[5];
 
-var d = parseInt(dia);
-var h = parseInt(hora);
-var m = parseInt(minuto);
-var s = parseInt(segundo);
+//PARSE DOS TEMPOS INICIAIS PARA INTEIRO
+var di = parseInt(dia);
+var hi = parseInt(hora);
+var mi = parseInt(minuto);
+var si = parseInt(segundo);
 
-var totalEmSegundo = h * 60 *60 + m *60 + s;
-var tempoRecheio = 86400-totalEmSegundo;
-d++;
+//CAPTURA DOS DADOS QUE QUANTIFICAM TEMPO NAS ENTRADAS DE TÉRMINO
+var hora = horaFinal[0]+horaFinal[1];
+var minuto = horaFinal[5]+horaFinal[6];
+var segundo = horaFinal[10]+horaFinal[11];
+
+var dia = diaFinal[4]+diaFinal[5];
+
+//PARSE DOS TEMPOS FINAIS PARA INTEIRO
+var df = parseInt(dia);
+var hf = parseInt(hora);
+var mf = parseInt(minuto);
+var sf = parseInt(segundo);
+
+
+var totalEmSegundoDodiaInicial = hi * 60 *60 + mi *60 + si;
+var tempoRecheioDodiaInicial = 86400-totalEmSegundoDodiaInicial;//quero
+di++;
+var qtdDias = df-di;
+var tempoEmSegundosDosDias = qtdDias*24*60*60;//quero
+var tempoEmSegundosDoDiaFinal = hf * 60 *60 + mf *60 + sf;//quero
+
+var totalSegundos = tempoRecheioDodiaInicial+tempoEmSegundosDosDias+tempoEmSegundosDoDiaFinal;
+
+var respostaDias = totalSegundos/86400;
+var respostaHoras = (totalSegundos%86400)/3600;
+var respostaMin = ((totalSegundos%86400)%3600)/60;
+var respostaSeg = ((totalSegundos%86400)%3600)%60;
+
+console.log(`${Math.trunc(respostaDias)} dia(s)`);
+console.log(`${Math.trunc(respostaHoras)} hora(s)`);
+console.log(`${Math.trunc(respostaMin)} minuto(s)`);
+console.log(`${Math.trunc(respostaSeg)} segundo(s)`);
+
+
 
 
